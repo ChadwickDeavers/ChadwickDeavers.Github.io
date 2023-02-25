18e8c17570c0e230ad89167a8cf845e51b0b4cdc
Chadwick Deavers <119131021+ChadwickDeavers@users.noreply.github.com>
2023-02-25 10:56:30 -0500

    reiko

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,7 @@
 
 import * as THREE from '/node_modules/three';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls';
 
 
 
